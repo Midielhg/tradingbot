7b66c4bf7bf590b369d89cbbc241f6fa25e243fb
Midiel Henriquez <71528908+Midielhg@users.noreply.github.com>
2024-05-15 14:47:17 +0000

    chore: Update cryptoTrader.py for improved market value calculation

--- cryptoTrader.py
+++ cryptoTrader.py
@@ -9,7 +9,6 @@ import numpy as np
 from datetime import datetime
 import time
 wb = webull()
-
 import robin_stocks.robinhood as rh
 import sys
 import os
@@ -23,44 +22,34 @@ crypto = "DOGE"
 #supper trend parameters
 period = 10
 factor = 1
-    
 
+# get positions
 crypto_positions = rh.get_crypto_positions()
+quote = rh.get_crypto_quote(crypto)
 
-# Initialize total market value
-total_market_value = 0
 
 # Print Positions
 for item in crypto_positions:
-    crypto_name = item['currency']['name']
-    # Check if 'cost_bases' is not empty
-    if item['cost_bases']:
-        # Assuming market value is in 'direct_cost_basis'
-        market_value = float(item['cost_bases'][0]['direct_cost_basis'])
-        # Add market value to total
-        total_market_value += market_value
-        if market_value >= 0.1:
-            print("Crypto Name: ", crypto_name, "Market Value: ", market_value)
- 
- 
-print("Total Market Value: ", total_market_value)
- 
-if total_market_value >= 0.1:
+    if item['currency']['code'] == crypto:
+        quantity = item['quantity']
+        print(crypto, "quantity: ", quantity)
+        # market_value = qurrency * quantity
+        market_value = float(item['quantity']) * float(quote['mark_price'])
+        print(crypto, "Position Market Value: $", market_value)
+
+            
+# print buying power
+account_info = rh.account.load_account_profile()
+buying_power = float(account_info['buying_power'])
+print("Buying Power: $", buying_power)
+
+# print if in long position
+if market_value >= 1:
     in_longPosition = True
     print("You are in a long position, looking for a place to sell")
 else:
     in_longPosition = False
     print("You are not in a long position, looking for a place to buy")
-            
-
-
-
-    
-# print buying power
-account_info = rh.account.load_account_profile()
-print("Buying Power: ", account_info['buying_power'])
-
-
 
 #indicators
 def tr(data):#true range
@@ -108,40 +97,67 @@ def supertrend(df, period, atr_multiplier): #supertrend
 def check_buy_sell_signals(df):
     global in_longPosition #global in_longPosition
 
-    print(df.tail(2)) #print the last 2 rows of the dataframe
+    # print(df.tail(2)) #print the last 2 rows of the dataframe
     last_row_index = len(df.index) - 1 #get the index of the last row
     previous_row_index = last_row_index - 1 #get the index of the previous row
-        
-    if not df['in_uptrend'][previous_row_index] and df['in_uptrend'][last_row_index]: #if the previous row was not in an uptrend and the last row is in an uptrend
-        print("changed to uptrend, Long")
-        if  in_longPosition == False: #if not on long position, buy
-            order = rh.order_buy_crypto_by_price(crypto, 50.0, timeInForce='gtc')
+    
+    #if the current row is in an uptrend
+    if df['in_uptrend'][last_row_index]:
+        print("Current Trend: UpTrend")
+        if in_longPosition == False:
+            order = rh.order_buy_crypto_by_price(crypto, buying_power, timeInForce='gtc')
             print("Buying ", crypto)
             print(order)
             in_longPosition = True
         else:
-            print("already in Long, nothing to do")
-        
-    if df['in_uptrend'][previous_row_index] and not df['in_uptrend'][last_row_index]:#if the previous row was in an uptrend and the last row is not in an uptrend
-        print ("changed to downtrend, Short")
-        if in_longPosition == True: #if is not on Short position Sell
-            order = rh.order_sell_crypto_by_price(crypto, 50.0, timeInForce='gtc')
+            print("already in Long, Making Money")
+    else:
+        print("Current Trend: DownTrend")
+        if in_longPosition == True:
+            order = rh.order_sell_crypto_by_price(crypto, quantity, timeInForce='gtc')   
             print("Selling", crypto)
             print(order)
             in_longPosition = False
         else:
-            print("already in Short, nothing to do")
+            print("Already out of the position, Saved from the DownTrend")
+
+    
+    # #if trend change from downtrend to uptrend, buy 
+    # if not df['in_uptrend'][previous_row_index] and df['in_uptrend'][last_row_index]: #if the previous row was not in an uptrend and the last row is in an uptrend
+    #     print("changed to uptrend, Long")
+    #     if  in_longPosition == False: #if not on long position, buy
+    #         order = rh.order_buy_crypto_by_price(crypto, buying_power, timeInForce='gtc')
+    #         print("Buying ", crypto)
+    #         print(order)
+    #         in_longPosition = True
+    #     else:
+    #         print("already in Long, nothing to do")
+        
+    # # if trend change from uptrend to downtrend, sell
+    # if df['in_uptrend'][previous_row_index] and not df['in_uptrend'][last_row_index]:#if the previous row was in an uptrend and the last row is not in an uptrend
+    #     print ("changed to downtrend, Short")
+    #     if in_longPosition == True: #if is not on Short position Sell
+    #         order = rh.order_sell_crypto_by_price(crypto, total_market_value, timeInForce='gtc')   
+    #         print("Selling", crypto)
+    #         print(order)
+    #         in_longPosition = False
+    #     else:
+    #         print("already in Short, nothing to do")
         
 #Run the bot  
 def run_bot():
-    
     print(f"\nFetching new bars for {datetime.now().isoformat()}")
     bars = wb.get_bars(stock=crypto, interval='m1', count=100, extendTrading=1) #get the last 100 bars of TQQQ at 1 minute intervals
     df = pd.DataFrame(bars[:-1], columns=['timestamp', 'open', 'high', 'low', 'close', 'volume']) #convert the fetched data into a pandas data-frame
-    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms') #convert the timestamp data to datetime format
-    supertrend(df, period, factor) #calculate the supertrend indicator
-    check_buy_sell_signals(df)#check for buy and sell signals
     
+    try:
+        df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms') #convert the timestamp data to datetime format
+    except FloatingPointError:
+        print("Invalid timestamp encountered. Please check the 'timestamp' column of your dataframe.")
+        return  # Skip the rest of the function if an error occurs
+
+    supertrend(df, period, factor) #calculate the supertrend indicator
+    check_buy_sell_signals(df)#check for buy and sell signals 
 
 schedule.every(3).seconds.do(run_bot) #run the bot every 3 seconds
 while True:
